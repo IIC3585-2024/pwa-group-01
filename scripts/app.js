80f3c955356b650ce8fef96a1d8a2c286b37b4f1
Manuel Pulgar <mfpulgar@uc.cl>
2024-05-06 15:54:09 -0400

    base

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+import db from "./db.js";
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
@@ -9,4 +11,108 @@ if ("serviceWorker" in navigator) {
         console.error("Service Worker: Error registering", err);
       });
   });
-};
+}
+
+addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("add");
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const table = document.getElementById("table");
+  const deleteAllButton = document.getElementById("delete-all-button");
+
+  let id = 1;
+
+  const removeNote = (id) => {
+    document.getElementById(id).remove();
+    db.deleteNoteById(id);
+  };
+
+  const addNote = () => {
+    if (title.value === "" || description.value === "") {
+      console.error("title and description are required");
+      return;
+    }
+
+    const row = table.insertRow();
+    row.setAttribute("id", id++);
+    row.innerHTML = `
+      <td>${title.value}</td>
+      <td>${description.value}</td>
+      <td class="check-container">
+        <input type="checkbox" />
+      </td>
+      <td>
+        <button class="edit-button">
+          <span class="material-symbols-outlined"> edit </span>
+        </button>
+      </td>
+    `;
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("delete-button");
+    removeBtn.innerHTML = `<span class="material-symbols-outlined"> delete </span>`;
+    row.children[3].appendChild(removeBtn);
+    removeBtn.onclick = (e) => {
+      removeNote(row.getAttribute("id"));
+    };
+
+    const note = {
+      id: row.getAttribute("id"),
+      title: title.value,
+      description: description.value,
+    };
+
+    db.addNote(note); // Llamada al método addNote de la clase DB para agregar la nota a la base de datos
+  };
+
+  btn.onclick = addNote;
+
+  // Función para cargar los elementos desde la base de datos
+  const cargar = () => {
+    console.log("entra a cargar");
+    db.getAllNotes((notes) => {
+      notes.forEach((note) => {
+        const row = table.insertRow();
+        row.setAttribute("id", note.id);
+        row.innerHTML = `
+          <td>${note.title}</td>
+          <td>${note.description}</td>
+          <td class="check-container">
+            <input type="checkbox" />
+          </td>
+          <td>
+            <button class="edit-button">
+              <span class="material-symbols-outlined"> edit </span>
+            </button>
+          </td>
+        `;
+        const removeBtn = document.createElement("button");
+        removeBtn.classList.add("delete-button");
+        removeBtn.innerHTML = `<span class="material-symbols-outlined"> delete </span>`;
+        row.children[3].appendChild(removeBtn);
+        removeBtn.onclick = (e) => {
+          removeNote(row.getAttribute("id"));
+        };
+      });
+    });
+  };
+
+  // Agregar un event listener al botón
+  deleteAllButton.addEventListener("click", () => {
+    db.getAllNotes((notes) => {
+      notes.forEach((note) => {
+        document.getElementById(note.id).remove();
+      });
+    });
+    db.deleteAllNotes(() => {
+      console.log("All notes deleted successfully");
+    });
+  });
+
+  // Abrir la base de datos y luego cargar los elementos
+  db._dbRequest.addEventListener("success", (event) => {
+    db.db = event.target.result;
+    console.log("Database opened");
+    cargar(); // Llamada a la función cargar después de abrir con éxito la base de datos
+  });
+});
--- scripts/db.js
+++ scripts/db.js
@@ -1,35 +1,108 @@
 class DB {
-    constructor(dbName, tableName) {
-        this.db;
-        this._init_db(dbName, tableName);
+  constructor(dbName, tableName) {
+    this.db;
+    this.currentId = 1;
+    this._init_db(dbName, tableName);
+  }
+
+  _init_db(dbName, tableName) {
+    this._dbRequest = indexedDB.open(dbName, 1);
+    this._dbRequest.addEventListener("error", (err) => {
+      console.error("Error opening database:", err);
+    });
+
+    this._dbRequest.addEventListener("success", (event) => {
+      this.db = event.target.result;
+      console.log("Database opened");
+    });
+
+    this._dbRequest.addEventListener("upgradeneeded", (event) => {
+      this.db = event.target.result;
+      this.db.onerror = (err) => {
+        console.error("Error creating database:", err);
+      };
+      this.db.createObjectStore(tableName, {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+
+      if (!this.db.objectStoreNames.contains("currentIdStore")) {
+        this.db.createObjectStore("currentIdStore", { keyPath: "id" });
+      }
+
+      console.log("Database created");
+    });
+  }
+
+  addNote(note) {
+    const transaction = this.db.transaction([tableName], "readwrite");
+    const objectStore = transaction.objectStore(tableName);
+    const request = objectStore.add(note);
+
+    request.onsuccess = function (event) {
+      console.log("Note added to database");
     };
 
-    _init_db(dbName, tableName) {
-        this._dbRequest = indexedDB.open(dbName, 1);
-        this._dbRequest.addEventListener('error', (err) => {
-            console.error('Error opening database:', err);
-        });
+    request.onerror = function (event) {
+      console.error("Error adding note to database:", event.target.error);
+    };
+  }
 
-        this._dbRequest.addEventListener('success', (event) => {
-            this.db = event.target.result;
-            console.log('Database opened');
-        });
+  getAllNotes(callback) {
+    const transaction = this.db.transaction([tableName], "readonly");
+    const objectStore = transaction.objectStore(tableName);
+    const request = objectStore.getAll();
 
-        this._dbRequest.addEventListener('upgradeneeded', (event) => {
-            this.db = event.target.result;
-            this.db.onerror = (err) => {
-                console.error('Error creating database:', err);
-            };
-            this.db.createObjectStore(tableName, { keyPath: 'id', autoIncrement: true });
-            console.log('Database created');
-        });
+    request.onsuccess = function (event) {
+      const notes = event.target.result;
+      callback(notes); // Llamar a la devolución de llamada con los datos recuperados
     };
 
-    // Add more methods here
+    request.onerror = function (event) {
+      console.error("Error getting notes from database:", event.target.error);
+    };
+  }
+
+  deleteAllNotes(callback) {
+    const transaction = this.db.transaction([tableName], "readwrite");
+    const objectStore = transaction.objectStore(tableName);
+    const request = objectStore.clear();
+
+    request.onsuccess = function (event) {
+      console.log("All notes deleted from database");
+      if (callback) {
+        callback();
+      }
+    };
+
+    request.onerror = function (event) {
+      console.error("Error deleting notes from database:", event.target.error);
+    };
+  }
+
+  deleteNoteById(id, callback) {
+    const transaction = this.db.transaction([tableName], "readwrite");
+    const objectStore = transaction.objectStore(tableName);
+    const request = objectStore.delete(id);
+
+    request.onsuccess = (event) => {
+      console.log(`Note with ID ${id} deleted successfully`);
+      if (callback) {
+        callback();
+      }
+    };
+
+    request.onerror = (event) => {
+      console.error(
+        `Error deleting note with ID ${id} from database:`,
+        event.target.error
+      );
+    };
+  }
 }
 
-const dbName = 'mydb';
-const tableName = 'notes';
+const dbName = "mydb";
+const tableName = "notes";
 
 const db = new DB(dbName, tableName);
 
