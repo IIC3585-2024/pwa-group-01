8395af4c0b511abd86cf739591792b2e31fc5c22
Leonardo Sergio Centellas Claros <47432876+leoncente@users.noreply.github.com>
2024-05-08 01:15:35 -0400

    basic service worker implemented

---
+++ README.md
@@ -0,0 +1,9 @@
+# PWA
+## Requsitos
+- Correr un server ya que se importan modulos de js
+- Si se usa un server con http se debe marcar la url como segura en el navegador
+
+Ej:
+
+En Chrome, en el enlace [chrome://flags/#unsafely-treat-insecure-origin-as-secure](chrome://flags/#unsafely-treat-insecure-origin-as-secure)
+se coloca [http://localhost:8000](http://localhost:8000) o la url deseada en `Insecure origins treated as secure` y se habilita
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,12 +1,38 @@
-import { assets, cacheName } from './swImports.json';
+const CACHE_NAME = 'v1';
 
-self.addEventListener('install', (installEvent) => {
-  console.log('Service Worker: Installing...');
-  installEvent.waitUntil(
-    caches.open(cacheName)
-      .then((cache) => {
-        console.log('Caching assets...');
-        cache.addAll(assets)
+const assets = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+];
+
+// Get all files within a folder and add them to the assest to cache
+async function listFilesRecursively(folder) {
+  const files = [];
+  const response = await fetch(folder);
+  const text = await response.text();
+  const linkRegex = /<a\s+(?:[^>]*?\s+)?href="([^"]*)"/g;
+  let match;
+  while ((match = linkRegex.exec(text)) !== null) {
+    const link = match[1];
+    const url = folder+link;
+    if (url.charAt(url.length - 1) === '/') {
+      files.push(...await listFilesRecursively(url));
+    } else {
+      files.push(url);
+    }
+  }
+  return files;
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(async (cache) => {
+        const filesToCache = [...assets];
+        filesToCache.push(...await listFilesRecursively('/scripts/'));
+        filesToCache.push(...await listFilesRecursively('/styles/'));
+        cache.addAll(filesToCache)
           .then(() => {
             console.log('Assets cached');
           })
@@ -14,8 +40,8 @@ self.addEventListener('install', (installEvent) => {
             console.error('Error adding assets to cache', err);
           });
       })
-      .catch((err) => {
-        console.error('Error opening cache', err);
+      .catch((error) => {
+        console.error('Error caching files:', error);
       })
   );
 });
