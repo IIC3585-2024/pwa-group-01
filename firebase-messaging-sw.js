9a11b5e7fe76cf5d1ec497df78a0b61f126b319f
Leonardo Sergio Centellas Claros <47432876+leoncente@users.noreply.github.com>
2024-05-10 15:37:06 -0400

    see if it works

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,6 +14,18 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+      body:  payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
+
 const CACHE_NAME = 'v1';
 
 const assets = [
