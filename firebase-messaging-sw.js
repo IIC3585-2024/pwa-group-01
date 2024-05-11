45c8ba33451c6994244eec86094511a6938d6fb1
Leonardo Sergio Centellas Claros <47432876+leoncente@users.noreply.github.com>
2024-05-10 23:49:32 -0400

    clean code

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -16,7 +16,6 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
       body:  payload.notification.body,
@@ -40,81 +39,21 @@ const assets = [
   "styles/style.css"
 ];
 
-// Get all files within a folder and add them to the assest to cache
-async function listFilesRecursively(folder) {
-  const files = [];
-  const response = await fetch(folder);
-  const text = await response.text();
-  const linkRegex = /<a\s+(?:[^>]*?\s+)?href="([^"]*)"/g;
-  let match;
-  while ((match = linkRegex.exec(text)) !== null) {
-    const link = match[1];
-    const url = folder+link;
-    if (url.charAt(url.length - 1) === '/') {
-      files.push(...await listFilesRecursively(url));
-    } else {
-      files.push(url);
-    }
-  }
-  return files;
-}
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(async (cache) => {
-        //const filesToCache = [...assets];
-        //filesToCache.push(...await listFilesRecursively('scripts/'));
-        //filesToCache.push(...await listFilesRecursively('styles/'));
-        //cache.addAll(filesToCache)
-        cache.addAll(assets)
-          .then(() => {
-            console.log('Assets cached');
-            //return self.skipWaiting()
-          })
-          .catch((err) => {
-            console.error('Error adding assets to cache', err);
-          });
-      })
-      .catch((error) => {
-        console.error('Error caching files:', error);
-      })
-  );
-});
-
-self.addEventListener('fetch', (fetchEvent) => {
-  console.log('Service Worker: Fetching...');
+self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request)
-      .then((res) => {
-        // Attempt to fetch a fresh copy from the network
-        const fetchPromise = fetch(fetchEvent.request)
-          .then((response) => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response to use it and store it in the cache
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(fetchEvent.request, responseToCache);
-              });
-
-            return response;
-          })
-          .catch((err) => {
-            console.error('Error fetching and caching new assets:', err);
-          });
-
-        // Return the cached response or the network response if available
-        return res || fetchPromise;
-      })
-      .catch((err) => {
-        console.error('Error fetching assets from cache:', err);
-      })
-  );
-});
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
