f2f7e8a236ffde185153514a11d23f7168017836
Leonardo Sergio Centellas Claros <47432876+leoncente@users.noreply.github.com>
2024-05-12 14:14:53 -0400

    icon added

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -19,6 +19,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
       body:  payload.notification.body,
+      icon: 'assets/icons/icon-72x72.png'
   };
 
   self.registration.showNotification(notificationTitle,
